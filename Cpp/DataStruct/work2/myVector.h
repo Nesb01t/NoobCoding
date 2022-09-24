8f4d5930687aedf817c9edb23f736376efb767ad
Nesb01t <littlelumos@foxmail.com>
2022-09-24 13:18:55 +0800

    update: 初始化调试成功

--- Cpp/DataStruct/work2/myVector.h
+++ Cpp/DataStruct/work2/myVector.h
@@ -1,5 +1,7 @@
 #define DEFAULT_CAPACITY 3
 
+#include <iostream>
+
 template <typename T>
 class myVector{
 protected:
@@ -14,9 +16,9 @@ protected:
     void bubbleSort(int left, int right);
 public:
     myVector(int c=DEFAULT_CAPACITY, int s=0, T v=0){ // 容量, 规模, 初始值
-        s>c?s=c:;
+        if(s>c)c=s;
         _capacity = c;_size = s;_elem = new T[_capacity];
-        for(size_t i=0;i<_capacity;i++)_elem[i]=v;
+        for(int i=0;i<_capacity;i++)_elem[i]=v;
     };
     myVector(T const* A, int n){
         _elem = new T[n+1];
@@ -31,7 +33,11 @@ public:
         delete[] _elem;
     };
 /* ---------- debugger ---------- */ 
-    debug_output(){for(size_t i=0;i<_size;i++)std::cout<<_elem[i]<<endl;}
+    void debug_output(){
+        for(int i=0;i<_size;i++){
+            std::cout<<_elem[i]<<std::endl;
+        }
+    }
 /* ---------- read only interface ---------- */
 
 
