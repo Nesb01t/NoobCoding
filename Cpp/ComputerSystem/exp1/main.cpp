78090b0624154cca1ccc385815f342a2b334adab
Littlelumos_portable <littlelumos@foxmail.com>
2022-09-30 19:54:38 +0800

    update: 16进制

--- Cpp/ComputerSystem/exp1/main.cpp
+++ Cpp/ComputerSystem/exp1/main.cpp
@@ -1,14 +1,24 @@
 #include "DecToBin.h"
+#include "DecToHex.h"
 using namespace std;
 
 int main(){
-    DecToBin num;
+    // 调试 DecToBin 的源码
+    // DecToBin num;
 
-    num.getDec();
+    // num.getDec();
 
-    num.toBin();
-    num.toDec();
-    num.printData();
+    // num.toBin();
+    // num.toDec();
+    // num.printData();
 
+    // 调试 DecToHex 的源码
+    DecToHex h;
+    h.getHex();
+    
+    h.toHex();
+    h.toDec();
+    
+    h.printData();
     return 0;
 }
