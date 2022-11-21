9bbfa9b5bf313ef9a4ce1e46cb379e4d06c961be
nesb01t <littlelumos@foxmail.com>
2022-11-22 01:41:13 +0800

    add: offer 03 start

--- Kotlin/src/main.kt
+++ Kotlin/src/main.kt
@@ -1,11 +1,7 @@
+import offer1.offer03
+
 fun main() {
-}
-
-fun getInput(): Any {
-    /*
-    输入部分
-     */
-    var input = 123
-
-    return input
+    val nums: IntArray = intArrayOf(2, 3, 1, 0, 2, 5, 3)
+    val ans = offer03.findRepeatNumber(nums)
+    println(ans)
 }
