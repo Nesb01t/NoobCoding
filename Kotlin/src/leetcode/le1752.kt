adee9abc0053ca58e86acac6f6778b22e0072908
nesb01t <littlelumos@foxmail.com>
2022-11-30 09:55:43 +0800

    update: commit

--- Kotlin/src/leetcode/le1752.kt
+++ Kotlin/src/leetcode/le1752.kt
@@ -2,6 +2,7 @@ package leetcode
 
 class le1752 {
     fun check(nums: IntArray): Boolean {
+        // 阅读理解题
         var count = 0
         for (i in nums.indices) {
             if (nums[i] > nums[(i + 1) % nums.size]) {
